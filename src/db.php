87dbb8ae59ca14f901c0584ab7f0e6a4b5f2e0c6
Dario Emerson <dedodj@gmail.com>
2019-02-05 18:15:35 +0100

    removed db.php as residual of previous merge

--- src/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$link = mysqli_connect($config['db_hostname'], $config['db_username'], $config['db_password']);
-if ($link != FALSE) {
-	mysqli_select_db($link, $config['db_name']); 
-} else {
-	echo('Could not connect to db');
-}
